7b3732acc7355cb50c89c6dd1b8fec13048f5517
Ruslanas Gžibovskis <lpic.lt@lpic.lt>
2022-05-18 11:49:38 +0300

    Create how-much-time-takes-to-generate-phrase.bash

---
+++ bash/how-much-time-takes-to-generate-phrase.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+KEYWORD="a"
+
+if [[ "$#" -eq "0" ]]; then
+	echo "$0 String-to-search"
+else
+	KEYWORD="$1"
+fi
+
+i=1
+a=0
+
+LETTERS=$(echo $KEYWORD | wc -m)
+LETTERS=$(($LETTERS -1))
+echo "LETTERS=${LETTERS} = ${KEYWORD}"
+
+while true
+do
+	TEST=$(cat /dev/urandom | tr -dc '[:print:]' | fold -w $LETTERS | head -n 1)
+	if [[ "$i" -eq "100000" ]]; then
+		((a++))
+		echo ${a}00000 $KEYWORD $TEST
+		sleep 5
+		i=0
+	fi
+	if [[ "$TEST" == "$KEYWORD" ]]; then
+		echo "${a}00000 + ${i} = $(echo "${a}00000 + ${i}" | bc ) times - to get KEYWORD=${KEYWORD} = ${TEST}=GENERATED WORD"
+		exit 0
+	fi
+	((i++))
+done
